f13a163c1b4ae886a14e1e4a320ff18290e276b0
Person-hub-lab <79429471+Person-hub-lab@users.noreply.github.com>
2021-12-18 23:10:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+random_number = random_no = Math.floor((Math.random()*array_1.length)+1);
+console.log(quick_draw_data_set = random_number);
+drawn_sketch = quick_draw_data_set = random_number;
+drawn = Sketch To be drawn+ drawn_sketch;
+
+
+timer_counter = 0;
+timer_check =  "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
+
